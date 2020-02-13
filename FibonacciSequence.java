76a8524084957a2e4674a91debe6f1281f701a72
Aarya Patel <aarya.patel@gmail.com>
2020-02-13 09:47:27 -0500

    Added iterativeMethod() to FibonacciSequence

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,28 +1,34 @@
 import java.util.function.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.stream.*;
 
 public class FibonacciSequence {
 		
 	private static BiFunction<List<Integer>, Integer, List<Integer>> recursive;
+	private static Function<Integer, List<Integer>> iterative;
 
 	static {
 		recursive = (a, b) -> {
-			int al = a.size();
-
-			if (al >= b)
+			if (a.size() >= b)
 				return a;
+			List<Integer> c = new ArrayList<Integer>(a);
+			c.add(a.get(a.size() - 1) + a.get(a.size() - 2));
+			return recursive.apply(c, b);
+		};
 
-			List<Integer> a2 = new ArrayList<Integer>(a);
-			a2.add(a.get(al - 1) + a.get(al - 2));
-
-			return recursive.apply(a2, b);
+		iterative = (a) -> {
+			List<Integer> b = new ArrayList<Integer>(List.of(0, 1));
+			for (; b.size() < a; b.add(b.get(b.size() - 1) + b.get(b.size() - 2)));
+			return b;
 		};
 	}
 
 	public static List<Integer> recursiveMethod(Integer length) {
 		return recursive.apply(List.of(0, 1), length);
+	}
+
+	public static List<Integer> iterativeMethod(Integer length) {
+		return iterative.apply(length);
 	}
 }
 
--- RecursionLab.java
+++ RecursionLab.java
@@ -1,6 +1,7 @@
 public class RecursionLab {
 	public RecursionLab() {
 		System.out.println("Fibonacci Sequence (Recursive): " + FibonacciSequence.recursiveMethod(10));
+		System.out.println("Fibonacci Sequence (Iterative): " + FibonacciSequence.iterativeMethod(10));
 	}
 
 	public static void main(String[] args) {
