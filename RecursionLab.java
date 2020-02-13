bd8d86ad0e27b330f028df08b330149d585201f5
Aarya Patel <aarya.patel@gmail.com>
2020-02-13 10:09:51 -0500

    Added Summation.java with recursive and iterative methods

--- RecursionLab.java
+++ RecursionLab.java
@@ -2,6 +2,9 @@ public class RecursionLab {
 	public RecursionLab() {
 		System.out.println("Fibonacci Sequence (Recursive): " + FibonacciSequence.recursiveMethod(10));
 		System.out.println("Fibonacci Sequence (Iterative): " + FibonacciSequence.iterativeMethod(10));
+		System.out.println("Summation (Recursive): " + Summation.recursiveMethod(10));
+		System.out.println("Summation (Iterative): " + Summation.iterativeMethod(10));
+	
 	}
 
 	public static void main(String[] args) {
---
+++ Summation.java
@@ -0,0 +1,29 @@
+import java.util.function.*;
+
+public class Summation {
+	private static BiFunction<Integer, Integer, Integer> recursive;
+	private static Function<Integer, Integer> iterative;
+
+	static {
+		recursive = (a, b) -> {
+			if (a == 0)
+				return b;
+			return recursive.apply(a - 1, b + a);
+		};	
+		
+		iterative = (a) -> {
+			int b = 0;
+			for(; a != 0; b += a--);
+			return b;
+		};
+	}
+
+	public static Integer recursiveMethod(int sumTo) {
+		return recursive.apply(sumTo, 0);
+	}
+
+	public static Integer iterativeMethod(int sumTo) {
+		return iterative.apply(sumTo);
+	}
+}
+
