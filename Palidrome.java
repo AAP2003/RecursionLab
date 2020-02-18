1331964ee8c302260710d1a3c04b782fc0ff5881
Aarya Patel <aarya.patel@gmail.com>
2020-02-18 08:27:31 -0500

    Fixed Palidrome Recursive Method

--- Palidrome.java
+++ Palidrome.java
@@ -8,23 +8,24 @@ public class Palidrome {
 		recursive = (a) -> {
 			return a.equals(" ") || a.length() < 2 ? true :
 				(Character.toLowerCase(a.charAt(0)) == Character.toLowerCase(a.charAt(a.length() - 1))
-					? recursive.test(a.substring(1, a.length() - 2)) : false);
+					? recursive.test(a.substring(1, a.length() - 1)) : false);
 		};
 
 		iterative = (a) -> {
-			boolean out = true;
-			for (int i = 0; i < (a.length() - a.length() % 2) / 2 && a.length() - 2 * i < 2; i++)
-				out = out && Character.toLowerCase(a.charAt(i)) == Character.toLowerCase(a.charAt(a.length() - i));
-			return out;
+			char[] chars = a.toCharArray();
+			for (int i = 0; a.length() - 2 * i < 2; i++)
+				if (chars[i] != chars[a.length() - (i + 1)])
+				       	return false;
+			return true;
 		};
 	}
 
 	public static boolean recursiveMethod(String checkPalidrome) {
-		return recursive.test(checkPalidrome);
+		return recursive.test(checkPalidrome.toLowerCase());
 	}
 
 	public static boolean iterativeMethod(String checkPalidrome) {
-		return iterative.test(checkPalidrome);
+		return iterative.test(checkPalidrome.toLowerCase());
 	}
 }
 
